b7829513aa7ecf9f83abefeb26e7abcf16884c6d
Tiago Ferreira <tiagojdferreira@gmail.com>
2017-02-01 21:17:38 +0100

    Add button split

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 'use strict'
+const { dialog } = require('electron').remote
 const fs = require('fs');
 const parser = require('csv').parse()
+
 const holder = document.getElementById('holder')
 
 const MAX_LINES = 100
@@ -14,19 +16,54 @@ holder.ondragleave = holder.ondragend = () => {
 holder.ondrop = (e) => {
   e.preventDefault()
   for (let f of e.dataTransfer.files) {
+    splitFile(f.path)
+  }
+  return false;
+}
+
+document.getElementById('file-loader').addEventListener('click', openAndSplitFile)
+function openAndSplitFile() {
+  openFile()
+  .then(splitFile)
+}
+
+function openFile() {
+  return new Promise((resolve, reject) => {
+    const config = {
+      properties: ['openFile'],
+      filters: [
+        {
+          name: 'csv',
+          extensions: ['csv'],
+        },
+      ],
+    }
+
+    return dialog.showOpenDialog(config, function (fileName) {
+      if (fileName == null) reject("You didn't save the file")
+
+      resolve(fileName)
+    })
+  })
+}
+
+function splitFile(fileNames) {
+  fileNames.forEach((fileName) => {
     let header;
-    const readStream = fs.createReadStream(f.path).pipe(parser)
+    const readStream = fs.createReadStream(fileName).pipe(parser)
     // fileStream.setEncoding('utf8');
     let lines = 1
     let files = 1
-    let writeStream = fs.createWriteStream(f.path.replace(f.name, `${files}.csv`))
+    let writeFile = fileName.replace(/(.*)\/.*(\.csv$)/i, `$1/${files}$2`)
+    let writeStream = fs.createWriteStream(writeFile)
     readStream.on('data', function(data) {
       if (header == null) header = data
       if (lines > MAX_LINES) {
         writeStream.end();
         lines = 1
         files++
-        writeStream = fs.createWriteStream(f.path.replace(f.name, `${files}.csv`))
+        let writeFile = fileName.replace(/(.*)\/.*(\.csv$)/i, `$1/${files}$2`)
+        writeStream = fs.createWriteStream(writeFile)
         writeStream.write(header.toString()+'\n')
         // console.log(header);
         // console.log(data);
@@ -37,8 +74,7 @@ holder.ondrop = (e) => {
     })
 
     readStream.on('end', function(data) {
-      console.log('end');
+      // writeStream.end()
     })
-  }
-  return false;
+  })
 }
